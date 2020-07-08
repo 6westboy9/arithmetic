c520cfadc5eaa9a1014d9700561d87998e8a60cd
6westboy9 <542229163@qq.com>
2020-07-08 22:30:30 +0800

    [新增]0424.替换后的最长重复字符

--- leetcode/src/main/java/q_0424/SolutionV1.java
+++ leetcode/src/main/java/q_0424/SolutionV1.java
@@ -10,6 +10,8 @@ public class SolutionV1 {
 
     }
 
+    // 左边界不动，右边界先前进（扩大窗口），什么时候停止呢？窗口内非重复元素个数小于 k 时，停止
+    // 右边界停止后，前进左边界（缩小窗口），什么时候停止呢？
     public int characterReplacement(String s, int k) {
 
         return 0;
